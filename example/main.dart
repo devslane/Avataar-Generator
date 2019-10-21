b0f934a71b3cae60630110709cd0a2fe8216e200
Kushagra Saxena <kkss420@gmail.com>
2019-10-21 14:28:33 +0530

    WIP for opening example svg in browser

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,28 @@
+import 'dart:io';
+
 import 'package:avataar_generator/generator.dart';
 
 void main() {
-  print(getSvg(new Options()));
+  final svgString = getSvg(new Options());
+
+  print(svgString);
+
+  runBrowser("data:text/html,<script>document.write('$svgString');</script>");
+}
+
+void runBrowser(String url) {
+  print(Platform.operatingSystem);
+  switch (Platform.operatingSystem) {
+    case "linux":
+      Process.run("x-www-browser", [url]);
+      break;
+    case "macos":
+      Process.run("open -a chrome", [url]);
+      break;
+    case "windows":
+      Process.run("explorer", [url]);
+      break;
+    default:
+      break;
+  }
 }
